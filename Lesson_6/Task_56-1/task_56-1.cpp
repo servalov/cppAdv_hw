f4c1e5e2d1d0e439e2bf184ed69c19c8e34e8c3d
Sergey Valov <sergey.valov.eng@gmail.com>
2025-07-24 00:28:32 +0300

    Добавлено домашнее задание по теме STL. часть2.

---
+++ Lesson_6/Task_56-1/task_56-1.cpp
@@ -0,0 +1,23 @@
+﻿// Занятие 6. STL. Часть 2. Задача 1. Уникальный вектор
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void print(int val)
+{
+	std::cout << val << " ";
+}
+
+int main()
+{
+	std::vector<int> in = { 1,1,2,5,6,1,2,4 };
+	std::cout << "[IN]: ";
+	for_each(in.begin(), in.end(), print);
+	std::sort(in.begin(), in.end());
+	auto new_end = unique(in.begin(), in.end());
+	in.erase(new_end, in.end());
+	std::cout << "\n[OUT]: ";;
+	for_each(in.begin(), in.end(), print);
+
+	return EXIT_SUCCESS;
+}
---
+++ Lesson_6/Task_56-2/task_56-2.cpp
@@ -0,0 +1,29 @@
+﻿// Занятие 6. STL. Часть 2. Задача 2. Печать контейнера
+#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+
+template <typename T>
+void print_container(const T& in)
+{
+	for (const auto& el : in)
+	{
+		std::cout << el << " ";
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container(test_set);
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list);
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector);
+
+	return EXIT_SUCCESS;
+}
