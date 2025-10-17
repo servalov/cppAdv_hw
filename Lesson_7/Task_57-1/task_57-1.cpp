4137f36dbdfbe6c927a1cab3cc0c3ced1e7077ef
Sergey Valov <sergey.valov.eng@gmail.com>
2025-10-17 15:21:52 +0300

    Добавлено домашнее задание к занятию "Умные указатели".

---
+++ Lesson_7/Task_57-1/task_57-1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class tridiagonal_matrix
+{
+public:
+    std::vector<double> m_down;
+    std::vector<double> m_upper;
+    std::vector<double> m_middle;
+    
+    tridiagonal_matrix(
+        const std::vector<double>& down,
+        const std::vector<double>& upper,
+        const std::vector<double>& middle) :
+        m_down{ down }, m_upper{ upper }, m_middle{ middle }
+    {};
+    ~tridiagonal_matrix() { std::cout << "destructor called\n"; }
+
+    // Функция клонирования объекта класса
+    std::unique_ptr<tridiagonal_matrix> clone()
+    {
+        return std::make_unique<tridiagonal_matrix>(this->m_down,this->m_upper,this->m_middle);
+    }
+    // clone()
+};
+
+void print(double a)
+{
+    std::cout << a << " ";
+}
+
+int main()
+{
+    auto down = std::vector<double>{ -4.0, 5.0 };
+    auto upper = std::vector<double>{ 14.0, 8.0 };
+    auto middle = std::vector<double>{ 3.0, 1.0, 7.0 };
+    auto matrix = std::make_unique<tridiagonal_matrix>(
+        down,
+        upper,
+        middle
+    );
+
+    auto matrix_clone = matrix->clone();
+
+    // Проверка. Вывод данных клонированного образа
+    std::cout << "Data of clone tridiagonal_matrix:" << std::endl;
+    std::for_each(matrix_clone->m_down.begin(), matrix_clone->m_down.end(), print);
+    std::cout << std::endl;
+    std::for_each(matrix_clone->m_upper.begin(), matrix_clone->m_upper.end(), print);
+    std::cout << std::endl;
+    std::for_each(matrix_clone->m_middle.begin(), matrix_clone->m_middle.end(), print);
+    std::cout << std::endl;
+
+};
+
---
+++ Lesson_7/Task_57-3/task_57-3.cpp
@@ -0,0 +1,67 @@
+﻿// Занятие 7. Умные указатели. Задание 3.std::unique_ptr своими руками.
+#include <iostream>
+
+template <typename T>
+class my_Unique_poiner
+{
+private:
+	T* data{ nullptr };
+
+public:
+	// Конструктор
+	my_Unique_poiner(T* m_data) :data{ m_data }
+	{
+		std::cout << "\nВызван конструктор my_Unique_poiner!!!" << std::endl;
+	}
+
+	// Деструктор
+	~my_Unique_poiner()
+	{
+		delete data;
+		std::cout << "\nВызван деструктор my_Unique_poiner!!!" << std::endl;
+	}
+
+	// Перегрузка оператора *
+	T& operator*()
+	{
+		if (!data)
+		{
+			throw std::runtime_error("Разыменование пустого указателя");
+		}
+		else
+		{
+			return *data;
+		}
+	}
+
+	// Запрещены оператор присваивания и конструктор копирования
+	my_Unique_poiner& operator=(const my_Unique_poiner& other) = delete;
+	my_Unique_poiner(const my_Unique_poiner& other) = delete;
+
+	// Метод release, который освобождает владение и возвращает сырой указатель
+	T* release()
+	{
+		T* old_data = data;
+		data = nullptr;              // Освобождение владения;
+		std::cout << "Владение объектом освобождено!!!" << std::endl;
+		return old_data;
+	}
+
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	my_Unique_poiner<int> my_ptr(new int(15));
+	std::cout << "Значение указателя объекта my_ptr равно " << *my_ptr << std::endl;
+
+
+	my_Unique_poiner<double> my_ptr2(new double(3.14f));
+	std::cout << "Значение указателя объекта my_ptr2 равно " << *my_ptr2 << std::endl;
+	double* ptr = my_ptr2.release();
+	std::cout << "Значение указателя ptr равно " << *ptr << std::endl;
+
+	delete ptr;   // Освобождение памяти для сырого указателя
+
+	return EXIT_SUCCESS;
+}
